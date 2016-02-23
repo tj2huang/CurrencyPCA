105bacd8625d069fc50fe69ecda4a3bfcd1ea7c9
tomhuang <tj2huang@uwaterloo.ca>
2016-02-23 18:18:19 -0500

    added rotation

--- pca/python/ManageData.py
+++ pca/python/ManageData.py
@@ -6,6 +6,7 @@ from matplotlib.backends.backend_pdf import PdfPages
 
 path = __file__[:__file__.find('python')]
 COMPONENTS = 5
+OUTLIER_PERCENT = 7
 
 
 def setup(data_path):
@@ -25,6 +26,7 @@ def get_components(data):
     pca = PCA(n_components=COMPONENTS)
     pca.fit(data)
     print(pca.components_, pca.explained_variance_ratio_)
+    # pca.components_ = ortho_rotation(pca.components_)
     transformed = pca.transform(data)
     df_transformed = pd.DataFrame(data=transformed,  index=data.index)
     return df_transformed
@@ -33,12 +35,39 @@ def get_components(data):
 def plot(df_transformed):
     pdf = PdfPages(path + "graph.pdf")
     plt.figure()
-    df_transformed.plot()
+    df_transformed.resample('W').plot()
     pdf.savefig()
     plt.close()
     pdf.close()
 
+
+def ortho_rotation(lam, method='varimax',gamma=None,eps=1e-6, itermax=100):
+    if gamma is None:
+        if method == 'varimax':
+            gamma = 1.0
+        if method == 'quartimax':
+            gamma = 0.0
+
+    nrow, ncol = lam.shape
+    R = np.eye(ncol)
+    var = 0
+
+    for i in range(itermax):
+        lam_rot = np.dot(lam, R)
+        tmp = np.diag(np.sum(lam_rot ** 2, axis=0)) / nrow * gamma
+        u, s, v = np.linalg.svd(np.dot(lam.T, lam_rot ** 3 - np.dot(lam_rot, tmp)))
+        R = np.dot(u, v)
+        var_new = np.sum(s)
+        if var_new < var * (1 + eps):
+            break
+        var = var_new
+
+    return R
+
 parsed_data = setup(path + '/data/USD crosses.csv')
-outliers = [filter(lambda x: abs(x[1]) >= 5, zip(parsed_data.index, parsed_data.ix[:, col])) for col in parsed_data.columns]
+outliers = [filter(lambda x: abs(x[1]) >= OUTLIER_PERCENT, zip(parsed_data.index, parsed_data.ix[:, col]))
+            for col in parsed_data.columns]
 ls_outliers = [pd.Series(data=[a[1] for a in x], index=[a[0] for a in x]) for x in outliers]
+merged = pd.concat(ls_outliers)
+parsed_data.drop(merged.index)
 plot(get_components(parsed_data))
